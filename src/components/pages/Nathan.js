e20c19e3b71c20ccf5e4012cf1ddd3dd9d727096
Nathan <usmc_wolf@hotmail.com>
2021-12-22 15:24:33 -0600

    added basic react template

---
+++ src/components/pages/Nathan.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Nathan extends Component {
+    redner() {
+        return(
+            <div>
+                <h1>Nathan's JS Page</h1>
+            </div>
+        )
+    }
+}
