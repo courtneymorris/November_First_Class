4d8268241363ba4cdef8596ce4b8f549d5ab4a39
Charlie3178 <charlielovejoy@bellsouth.net>
2021-12-22 17:50:30 -0600

    Named default class 'Charlie'

--- src/components/pages/charlie/charlie.js
+++ src/components/pages/charlie/charlie.js
@@ -3,44 +3,74 @@ import Temporal from "temporal";
 import moment from "moment";
 
 
-export default class extends Component {
+export default class Charlie extends Component {
     constructor() {
         super();
     }
     render() {
         return(
         <div className="charlie-wrapper">
-            <div className="character-data-inputs">
-                <h4>Player Name</h4>
-                <input type="text" id="PlayerName" label="Player Name:" />
-                <h4>Character Name</h4>
-                <input type="text" id="CharacterName" />
-                <h4>Character Level</h4>
-                <input type="text" id="CharacterLevel" />
-                <h4>Gender</h4>
-                <input type="text" id="Gender" />
-                <h4>Race</h4>
-                <input type="text" id="Race" />
-                <h4>Age</h4>
-                <input type="text" id="Age" />
-                <h4>Height</h4>                
-                <input type="text" id="Height" />
-                <h4>Weight</h4>                
-                <input type="text" id="Weight" />
-                <h4>Hair Color/Description</h4>                
-                <input type="text" id="HairColor" />
-                <h4>Eye Color/Description</h4>                
-                <input type="text" id="EyeColor" />
-                <h4>Skin Color/Description</h4>                
-                <input type="text" id="SinkColor" />
-                <h4>Class(es)</h4>                
-                <input type="text" id="Class" />
-                <h4>Level(s)</h4>                
-                <input type="text" id="Level" />
-                <h4>Alignment</h4>            
-                <input type="text" id="Alignment" />
-                <h4>Diety/Cause</h4>                
-                <input type="text" id="Diety/Cause" />
+            <div className="character-data">
+                <div>
+                    <h4>Player Name</h4>
+                    <input type="text" id="PlayerName" label="Player Name:" />
+                </div>
+                <div>
+                    <h4>Character Name</h4>
+                    <input type="text" id="CharacterName" />
+                </div>
+                <div>
+                    <h4>Character Level</h4>
+                    <input type="text" id="CharacterLevel" />
+                </div>
+                <div>
+                    <h4>Gender</h4>
+                    <input type="text" id="Gender" />
+                </div>
+                <div>
+                    <h4>Race</h4>
+                    <input type="text" id="Race" />
+                </div>
+                <div>
+                    <h4>Age</h4>
+                    <input type="text" id="Age" />
+                </div>
+                <div>
+                    <h4>Height</h4>                
+                    <input type="text" id="Height" />
+                </div>
+                <div>
+                    <h4>Weight</h4>                
+                    <input type="text" id="Weight" />
+                </div>
+                <div>
+                    <h4>Hair Color/Description</h4>
+                    <input type="text" id="HairColor" />
+                </div>
+                <div>
+                    <h4>Eye Color/Description</h4>                
+                    <input type="text" id="EyeColor" />
+                </div>
+                <div>
+                    <h4>Skin Color/Description</h4>                
+                    <input type="text" id="SinkColor" />
+                </div>
+                <div>
+                    <h4>Class(es)</h4>                
+                    <input type="text" id="Class" />
+                </div>
+                <div>
+                    <h4>Level(s)</h4>                
+                    <input type="text" id="Level" />
+                </div>
+                <div>
+                    <h4>Alignment</h4>            
+                    <input type="text" id="Alignment" />
+                </div>
+                <div>
+                    <h4>Diety/Cause</h4>                
+                    <input type="text" id="Diety/Cause" />
+                </div>
             </div>
             <div className="character-data-labels"></div>
             <div className="ability-scores">
