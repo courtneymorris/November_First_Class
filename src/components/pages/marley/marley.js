39c1d49cbd1703812af790b488bb4d75414a2ee2
AkoMarley99 <akomarley@yahoo.com>
2021-12-22 16:10:02 -0700

    HandleInput function and handleSubmit function

--- src/components/pages/marley/marley.js
+++ src/components/pages/marley/marley.js
@@ -1,13 +1,33 @@
 import React, {Component} from "react"
 import {NavLink} from "react-router-dom"
 
-
-
 export default class Marley extends Component{
+    constructor(props){
+    super(props)
+    this.state = {value: ' '};
+    this.state = {toDolist:' '}
+    }
+    
+
+    handleInput(event) {
+        this.state({value: event.target.value})
+    }
+
+    handleSubmit (event) {
+        alert("Task added to Todo list")
+        value.toDoList.map((input.value))
+
+    }
+
+
     render(){
-        return
-        <div className="marley-wrapper">
-            <h1>Ako Marley </h1>
-        </div>
+        return (
+            <><><h1>ToDo list</h1>
+                <input type="text" value={this.state.value} onSubmit={this.handleInput} placeholder="Type task here..." /></>
+                <button onClick={value.handleSubmit}>Submit</button></>
+        )
+       
     }
 }
+
+
