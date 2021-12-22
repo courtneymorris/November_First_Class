dc8822372c7fbacb5020d118d37abf91cac071ee
effervescent000 <kislio01@gmail.com>
2021-12-22 17:57:13 -0500

    added testChild component to tara.js

--- src/components/pages/tara/tara.js
+++ src/components/pages/tara/tara.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 
+import TestChild from "./test-child";
+
 export default class TaraComponent extends Component {
     render() {
         return (
             <div className="tara-wrapper">
                 <h1>Some stuff</h1>
+                <TestChild backgroundColor="pink" />
             </div>
-        )
+        );
     }
 }
--- src/components/pages/tara/test-child.js
+++ src/components/pages/tara/test-child.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function(props) {
     return (
-        <div className="child-div" style={{backgroundColor: props.backgroundColor}}>
+        <div style={{backgroundColor: props.backgroundColor}}>
             <span>Some text to fill this out</span>
         </div>
-    )
+    );
 }
